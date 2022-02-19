69b5d3bed4acdc6667dc0d1d690d57f76f69d601
mnoah1 <92764374+mnoah1@users.noreply.github.com>
2022-02-19 16:08:18 -0500

    Worked on front-end functionality
    Co-authored-by: rsa0810 <rsa0810@users.noreply.github.com>

---
+++ client/components/MarbleJar.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import * as actions from '../actions/actions';
+
+// const initialUser =  {isLoggedIn: false, userId: null, firstName: null, username: '', password:  '', errorMessage: ''};
+
+// const initialState = {
+//   loginData: initialUser,
+//   goalsData: [],
+//   classStatus: {totalPending: 0, totalComplete: 0}, 
+//   studentStatus: {totalPending: 0, totalComplete: 0}
+// }
+
+class MarbleJar extends Component {
+  render() {
+    return(
+      <div>Class Status</div>
+      );      
+  }
+}
+
+export default MarbleJar;
---
+++ client/reducers/reducer.js
@@ -0,0 +1,82 @@
+import * as types from '../constants/actionTypes';
+
+const initialUser =  {isLoggedIn: false, userId: null, firstName: null, username: '', password:  '', errorMessage: ''};
+// const initialUser =  {isLoggedIn: true, userId: null, firstName: 'Michael', username: '', password:  '', errorMessage: ''};
+
+// Comment out 
+const sampleGoalsData = [{description: "Read 3 books", due_date: '2021-02-15', status: 'Pending', _id: 'abcd'},
+ {description: "No talking during class", due_date: '2021-02-15', status: 'Pending', _id: 'abcd'},
+ {description: "Clean up after lunch", due_date: '2021-02-15', status: 'Pending', _id: 'abcd'},
+]
+
+
+const initialState = {
+  loginData: initialUser,
+  goalsData: sampleGoalsData,
+  classStatus: {totalPending: 0, totalComplete: 0}, 
+  studentStatus: {totalPending: 0, totalComplete: 0}
+}
+
+const reducer = (state = initialState, action) => {
+  console.log(state);
+  switch (action.type) {
+    case types.LOGIN_FIELD_CHANGE: {
+      let loginData = {...state.loginData};
+      loginData[action.payload.changedField] = action.payload.newValue;
+      
+      return {
+        ...state,
+        loginData
+      };
+    }
+
+
+    case types.LOGGED_IN: {
+      let loginData = {...state.loginData};
+      loginData.username = '';
+      loginData.password = '';
+      loginData.isLoggedIn = action.payload.isLoggedIn;
+      loginData.userId = action.payload.userId;
+      loginData.firstName = action.payload.firstName;
+      loginData.errorMessage = action.payload.errorMessage;
+
+      return { 
+        ...state,
+        loginData
+      };
+    }
+    case types.LOGGED_OUT: {
+      let loginData = {...initialUser};
+      return {
+        ...state,
+        loginData
+      };
+    }
+    case types.UPDATE_GOAL: {
+
+      let goalsData = [...state.goalsData];
+      goalsData[action.payload.index] = action.payload.response;
+
+      return {
+        ...state,
+        goalsData
+      };
+    }
+    case types.GET_GOALS: {
+      return {
+        ...state
+      };
+    }
+    case types.GET_CLASS: {
+      return {
+        ...state
+      };
+    }
+
+    default: {
+      return state;
+    }
+}
+}
+
+export default reducer;
