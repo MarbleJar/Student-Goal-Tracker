7ab84ccf26f2fb681d0bf1ee6ace23ed55755b7b
ruslanovna <ruslanovna.kim@gmail.com>
2022-02-18 21:11:23 -0500

    set up boiler plate files

---
+++ client/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+// import App from './components/App';
+
+// so that webpack can bundle styles 
+// import style from './style.css';
+
+render(
+  // render main top component App
+//   <App />,
+  // attach the index.js on HTML by the root
+  document.getElementById('root')
+);
+
