74d6fba3615e86d73b674662a5a1c556d972a8e7
ruslanovna <ruslanovna.kim@gmail.com>
2022-02-19 15:52:26 -0500

    create MondoDB connection for fetching and storing data

--- server/models/studentModels.js
+++ server/models/studentModels.js
@@ -1,1 +1,34 @@
 const mongoose = require('mongoose');
+
+// create a constructor 
+const Schema = mongoose.Schema;
+
+// create a student schema
+const studentSchema = new Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  userName: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+});
+
+// create a collection instance of the model
+const Student = mongoose.model('students', studentSchema);
+
+
+// create a goal schema
+const goalsSchema = new Schema({
+  description: {type: String, required: true}, 
+  dueDate: Number,
+  //behaves like a foreign key referencing the 'students' collection
+  status: {
+    type: Schema.Types.ObjectId,
+    ref: 'students'
+  }
+});
+
+// create a collection instance of the model 
+const Goals = mongoose.model('goals', goalsSchema);
+
+
+module.exports = {Student, Goals};
+
